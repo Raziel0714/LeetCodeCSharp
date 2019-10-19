b581fbfc0ffdfb3d89eddc837ed6490b2ac39719
Saber <7huang14@gmail.com>
2019-10-19 00:02:18 -0400

    3 longest substring without repeating chars

---
+++ 3Longest Substring Without Repeating Characters.cs
@@ -0,0 +1,24 @@
+/**
+* This question is required to find the longest substring without repeating. Two pointers come to my mind directly, use one pointer to locate the start point of the string, and move end pointer when the substring doesn't contain repeat character. if we find a repeat character, move start to avoid.
+* how to check the substring, and move the substring is a question.
+*/
+
+
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        if(s == null) return 0;
+        if(s.Length == 1) return 1;
+        int ans = 0;
+        int start = 0; int end = 1;
+        while(end < s.Length){
+            if(s.Substring(start, end - start).Contains(s[end])){
+                start++;
+            }else{
+                end++;
+            }
+            Console.WriteLine(start+":"+end);
+            ans = Math.Max(ans, end-start);
+        }
+        return ans;
+    }
+}
